392f22facdfeec3538f698e43ffc05f9dadb5f90
Andrey Ruts <ravnb1989@gmail.com>
2024-11-11 20:46:28 +0200

    adj hw 12.6

--- src/main.js
+++ src/main.js
@@ -19,53 +19,52 @@ async function loadMore(event) {
   event.preventDefault();
   page += 1;
   await handleRequest();
-  
+
   const card = document.querySelector(".list-item");
   const cardHeight = card.getBoundingClientRect().height;
+  
   window.scrollBy({
     left: 0,
     top: cardHeight * 2,
     behavior: "smooth"
-  })
+}) 
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const requestValue = event.target.elements.request.value;
+
   if (requestValue === "") {
     return;
   }
-  // page = 1;
+
+  page = 1;
   tempReqValue = requestValue;
   gallery.innerHTML = "";
   handleRequest();
 }
 
 async function handleRequest() {
+  loadMoreBtn.style.display = "none";
   loader.style.display = "block";
   const data = await fetchData(tempReqValue, page)
-  // console.log(data);
-  // console.log(page);
-  // console.log(data.totalHits / per_page);
-  // console.log(page >= (data.totalHits / per_page));
-  
   const prepPic = data.hits;
+
   if (prepPic.length === 0) {
     loadMoreBtn.style.display = "none";
     iziToast.error({
       message: 'Sorry, there are no images matching your search query. Please try again!',
       position: 'center',
     });
-  } if (page >= (data.totalHits / per_page)) {
-    console.log("some text");
-    iziToast.info({ message: "We're sorry, but you've reached the end of search results."})
+  }
+  if (page >= (data.totalHits / per_page)) {
+    iziToast.info({ message: "We're sorry, but you've reached the end of search results." });
     loadMoreBtn.style.display = "none";
     loader.style.display = "none";
   } else {
     loadMoreBtn.style.display = "block";
   }
-  
-  loader.style.display = "none";
-  renderData(data.hits);
-  document.querySelector(".form").reset();
+    loader.style.display = "none";
+    renderData(data.hits);
+    document.querySelector(".form").reset();
 }
